6731e125088e570c6d2a8093d230247b137283ae
manishfw13 <manish_fw13_347@masai.school>
2022-08-28 11:33:52 +0530

    change3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ const topWatch = require("./controllers/TopWatchController")
 const app = express()
  
 app.use(express.json())
- 
+app.get("/", async(req, res) => {
+    try{
+        res.status(200).send("Welcome to the clone of  boat")
+    }
+    catch(err) {
+        res.status(400).send(err)
+    }
+})
+
  
 app.use("/topwatch", topWatch)
  
